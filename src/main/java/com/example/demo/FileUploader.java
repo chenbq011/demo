a16fb1f2bb75b3ebc707da2d272e9c7739b29377
Administrator <Administrator@Z4CUVCXE6MT5AQR>
2018-02-13 11:18:50 +0800

    完成简单的文件上传功能

---
+++ src/main/java/com/example/demo/FileUploader.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 文件上传测试
+ * @author chenbq
+ * 2017-02-13
+ */
+
+@Controller
+public class FileUploader {
+	
+	private static Logger log = LoggerFactory.getLogger(FileUploader.class);
+
+	@RequestMapping("choose_file")
+	public String chooseFile() {
+		return "choose_file";
+	}
+	
+	@RequestMapping("upload")
+	@ResponseBody
+	public String upload(@RequestParam("file") MultipartFile file) {
+		String msg = "";
+		if (!file.isEmpty()) {
+			String fileName = "e:/mylab/" + file.getOriginalFilename();
+			log.info("file name:" + fileName);
+			log.info("file content type:" + file.getContentType());
+			log.info("file size:" + file.getSize());
+			try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fileName))) {
+				out.write(file.getBytes());
+				out.flush();
+			} catch(IOException e) {
+				log.info("保存文件失败");
+			}
+			
+			msg = "上传成功";
+			log.info(msg);
+		} else {
+			msg = "上传失败，文件为空！";
+			log.info(msg);
+		}
+		
+		return msg;
+	}
+	
+	
+}

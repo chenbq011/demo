0cd7e75fae94b01fc0fe593901e59f0dc956d5f9
Administrator <Administrator@Z4CUVCXE6MT5AQR>
2018-02-24 16:46:10 +0800

    Hibernate分表

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.bo.EmployeeBO;
 import com.example.demo.dao.IEmployeeDAO;
+import com.example.demo.repository.EmployeeRepository;
 
 @Controller
 public class Hello {
@@ -12,9 +14,18 @@ public class Hello {
 	@Autowired
 	private IEmployeeDAO dao;
 	
+	@Autowired
+	private EmployeeRepository respository;
+	
 	@RequestMapping("/")
 	public String hello() {
 		dao.listEmployee();
+		EmployeeBO emp = new EmployeeBO();
+		emp.setName("ccc");
+		emp.setGender("x");
+		respository.save(emp);
+		
+		
 		return "hello";
 	}
 	
